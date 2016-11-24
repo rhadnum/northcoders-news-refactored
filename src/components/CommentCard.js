8f329471dfc635bf29601756484ae5ab30d2d1bc
Rohan Hadnum <rhadnum@gmail.com>
2016-11-24 16:20:52 +0000

    Add conditional rendering for user upvote and downvote buttons

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class CommentCard extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hasVoted : 0
+    }
+  }
   render () {
+    console.log(this.state)
     return (
       <div className='box'>
         <p className='comment-body'>{this.props.comment.body}</p>
         <p className='comment-subtext'>
           Posted <span>{this.formatDate()}</span> by <a href='#'>{this.props.comment.created_by}</a>
         </p>
+        <p>
+          <span>Votes: {this.props.comment.votes + this.state.hasVoted}</span>
+          <span className={`icon ${this.state.hasVoted === 1 ? 'vote-up' : ''}`}><i className="fa fa-thumbs-up"></i></span>
+          <span className={`icon ${this.state.hasVoted === -1 ? 'vote-down' : ''}`}><i className="fa fa-thumbs-down"></i></span>
+        </p>
+
       </div>
     );
   }
