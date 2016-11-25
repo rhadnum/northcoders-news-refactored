e0e5a8630ee9b5a90b9771c62d7197a4c701757c
Rohan Hadnum <rhadnum@gmail.com>
2016-11-25 15:18:40 +0000

    Add article upvote and downvote put requests but doesn't update UI

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,30 +1,66 @@
 import React from 'react';
 import {Link} from 'react-router';
+import request from 'superagent';
+import {ROOT} from '../../config';
 
-const ArticleCard = function (props) {
-  return (
-    <div className='box'>
-      <article className='media article-card'>
-        <div className='media-left'>
-          <p><i className="fa fa-angle-up fa-3x"></i></p>
-          <p>Upvotes:</p>
-          <p>{props.article.votes}</p>
-          <p><i className="fa fa-angle-down fa-3x"></i></p>
-        </div>
-        <div className='media-content'>
-          <div className='content'>
-            <Link to={`/article/${props.article._id}`}>
-              <h3 className='title is-3'>{props.article.title}</h3>
-            </Link>
-            <p className='comment-subtext'>
-              Posted by <a href='#'>{props.article.created_by}</a> in <Link to={`${props.article.belongs_to}`}>{props.article.belongs_to}</Link>
-            </p>
-            <p className='comment-subtext'>{props.article.comments} comments</p>
+class ArticleCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasVoted: 0
+    };
+    this.voteUp = this.voteUp.bind(this);
+    this.voteDown = this.voteDown.bind(this);
+  }
+  voteUp () {
+    if (this.state.hasVoted !== 0) return;
+    this.setState({
+      hasVoted: 1
+    });
+    request
+      .put(`${ROOT}/articles/${this.props.article._id}?vote=up`)
+      .end(function (err, res) {
+        console.log(err)
+        console.log(res)
+      });
+  }
+
+  voteDown () {
+    if (this.state.hasVoted !== 0) return;
+    this.setState({
+      hasVoted: -1
+    });
+    request
+      .put(`${ROOT}/articles/${this.props.article._id}?vote=down`)
+      .end();
+  }
+  render() {
+    return (
+      <div className='box'>
+        <article className='media article-card'>
+          <div className='media-left'>
+            <p onClick={this.voteUp}><i className="fa fa-angle-up fa-3x"></i></p>
+            <p>Upvotes:</p>
+            <p>{this.props.article.votes}</p>
+            <p onClick={this.voteDown}><i className="fa fa-angle-down fa-3x"></i></p>
           </div>
-        </div>
-      </article>
-    </div>
-  );
-};
+          <div className='media-content'>
+            <div className='content'>
+              <Link to={`/article/${this.props.article._id}`}>
+                <h3 className='title is-3'>{this.props.article.title}</h3>
+              </Link>
+              <p className='comment-subtext'>
+                Posted by <a href='#'>{this.props.article.created_by}</a> in <Link
+                to={`${this.props.article.belongs_to}`}>{this.props.article.belongs_to}</Link>
+              </p>
+              <p className='comment-subtext'>{this.props.article.comments} comments</p>
+            </div>
+          </div>
+        </article>
+      </div>
+    );
+  };
+}
 
 export default ArticleCard;
